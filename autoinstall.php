60f11cc645a475d1dde0f330fdd5167ef63fa90a
Ben <hostellerie.org@gmail.com>
2026-08-17 09:14:11 +0200

    Register Store 0.7.0 commerce tables

--- autoinstall.php
+++ autoinstall.php
@@ -2,30 +2,19 @@
 
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Store Plugin 0.6.5                                                       |
+// | Store Plugin 0.7.0                                                       |
 // +---------------------------------------------------------------------------+
 // | autoinstall.php                                                          |
-// |                                                                          |
-// | Geeklog automatic installation metadata for the Store plugin.            |
+// |                                                                           |
+// | Geeklog automatic installation metadata for the Store plugin.             |
 // +---------------------------------------------------------------------------+
 // | Copyright (C) 2026 by Geeklog Store contributors                         |
 // +---------------------------------------------------------------------------+
-// |                                                                          |
-// | This program is free software; you can redistribute it and/or            |
-// | modify it under the terms of the GNU General Public License              |
-// | as published by the Free Software Foundation; either version 2           |
-// | of the License, or (at your option) any later version.                   |
-// |                                                                          |
-// | This program is distributed in the hope that it will be useful,          |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
-// | GNU General Public License for more details.                             |
-// |                                                                          |
-// | You should have received a copy of the GNU General Public License        |
-// | along with this program; if not, write to the Free Software              |
-// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA                |
-// | 02111-1307, USA.                                                         |
-// |                                                                          |
+// |                                                                           |
+// | This program is free software; you can redistribute it and/or             |
+// | modify it under the terms of the GNU General Public License               |
+// | as published by the Free Software Foundation; either version 2            |
+// | of the License, or (at your option) any later version.                    |
 // +---------------------------------------------------------------------------+
 
 function plugin_autoinstall_store($pi_name)
@@ -38,7 +27,7 @@ function plugin_autoinstall_store($pi_name)
         'info' => array(
             'pi_name'         => $pi_name,
             'pi_display_name' => $pi_display_name,
-            'pi_version'      => '0.6.5',
+            'pi_version'      => '0.7.0',
             'pi_gl_version'   => '2.1.1',
             'pi_homepage'     => 'https://github.com/Geeklog-Plugins/store'
         ),
@@ -59,7 +48,14 @@ function plugin_autoinstall_store($pi_name)
             'store_product_images',
             'store_orders',
             'store_order_items',
-            'store_payments'
+            'store_payments',
+            'store_tax_classes',
+            'store_tax_zones',
+            'store_tax_zone_locations',
+            'store_tax_rates',
+            'store_shipping_zones',
+            'store_shipping_zone_locations',
+            'store_shipping_methods'
         )
     );
 }
